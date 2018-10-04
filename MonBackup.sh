4c7e3ef1e3b745765511ac454f8a6fa65b1e6eb2
zebulon974 <zebulonfr@hotmail.fr>
2018-10-04 13:23:00 +0200

    Ajout du script MonBackup.sh depuis Arch_work

---
+++ MonBackup.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+#Les fichiers à sauvergarder
+if [ ! -d "/home/jeanlouis/Documents/ma_config" ];then
+echo "Création du dosser ma_config !";
+mkdir /home/jeanlouis/Documents/ma_config/
+else 
+	echo "Le dossier de backup existe dejà !"
+fi
+
+cp /home/jeanlouis/.zshrc /home/jeanlouis/Documents/ma_config/zshrc &
+cp /home/jeanlouis/.zsh_history /home/jeanlouis/Documents/ma_config/zsh-history &
+cp /home/jeanlouis/.vimrc /home/jeanlouis/Documents/ma_config/vimrc &
+
+# Copie du dossier custom de Oh-my-zsh"
+if [ ! -d "/home/jeanlouis/Documents/ma_config/custom" ];then
+	cp -r /home/jeanlouis/.oh-my-zsh/custom/ /home/jeanlouis/Documents/ma_config/ &
+else
+	echo "Le Dossier Custom existe déjà."
+fi
+
+echo "Sauvegarde Treminée!!"
