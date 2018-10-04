c1b29d508607369fa80c1d29ece0f9884a9d3f9f
zebulon974 <zebulonfr@hotmail.fr>
2018-10-04 22:11:24 +0200

    Ajout des alias pour yayà mes alias

--- custom/jlh.plugin.zsh
+++ custom/jlh.plugin.zsh
@@ -40,3 +40,20 @@ function cdl() {
 	cd $1
 	ls
 }
+
+# Alias pour YAY
+alias yaconf='yay -Pg'
+alias yaupg='yay -Syu'
+alias yasu='yay -Syu --noconfirm'
+alias yain='yay -S'
+alias yains='yay -U'
+alias yare='yay -R'
+alias yarem='yay -Rns'
+alias yarep='yay -Si'
+alias yareps='yay -Ss'
+alias yaloc='yay -Qi'
+alias yalocs='yay -Qs'
+alias yalst='yay -Qe'
+alias yaorph='yay -Qtd'
+alias yainsd='yay -S --asdeps'
+alias yamir='yay -Syy'
