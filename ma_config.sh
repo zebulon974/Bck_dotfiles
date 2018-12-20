06ec1e8af6dfc4bb7851f27766cdeba319dd86f9
zebulon974 <zebulonfr@hotmail.fr>
2018-12-20 13:20:46 +0100

    Modification de l'emplacements de mes dossiers de sauvegardes

--- ma_config.sh
+++ ma_config.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 
 #Les fichiers à sauvergarder
-if [ ! -d "${HOME}/Documents/dotfiles" ];then
-    echo "Création du dosser dotfiles !";
-    mkdir ${HOME}/Documents/dotfiles/
+if [ ! -d "${HOME}/Documents/mes_projets/dotfiles" ];then
+    echo "Création du dosser mes_projets/dotfiles !";
+    mkdir ${HOME}/Documents/mes_projets/dotfiles/
 else
 	echo "Le dossier de dotfiles existe dejà !"
 fi
 
-cp ${HOME}/.zshrc ${HOME}/Documents/dotfiles/zshrc &
-cp ${HOME}/.zsh_history ${HOME}/Documents/dotfiles/zsh-history &
-cp ${HOME}/.vimrc ${HOME}/Documents/dotfiles/vimrc &
+cp ${HOME}/.zshrc ${HOME}/Documents/mes_projets/dotfiles/zshrc &
+cp ${HOME}/.zsh_history ${HOME}/Documents/mes_projets/dotfiles/zsh-history &
+cp ${HOME}/.vimrc ${HOME}/Documents/mes_projets/dotfiles/vimrc &
 
 # Copie du dossier custom de Oh-my-zsh"
-if [ ! -d "${HOME}/Documents/dotfiles/custom" ];then
-	cp -r ${HOME}/.oh-my-zsh/custom/ ${HOME}/Documents/dotfiles/ &
+if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/custom" ];then
+	cp -r ${HOME}/.oh-my-zsh/custom/ ${HOME}/Documents/mes_projets/dotfiles/ &
 else
 	echo "Le Dossier Custom existe déjà"
 fi
