1032f08e06b9ffe4dd422ca5e5ebf57bedf7352b
zebulon974 <zebulonfr@hotmail.fr>
2018-12-20 16:54:12 +0100

    Ajout dans le fuichier config de la config de terminator

--- ma_config.sh
+++ ma_config.sh
@@ -5,12 +5,13 @@ if [ ! -d "${HOME}/Documents/mes_projets/dotfiles" ];then
     echo "Création du dosser mes_projets/dotfiles !";
     mkdir ${HOME}/Documents/mes_projets/dotfiles/
 else
-	echo "Le dossier de dotfiles existe dejà !"
+	echo "Le dossier dotfiles existe dejà !"
 fi
 
 cp -v -u ${HOME}/.zshrc ${HOME}/Documents/mes_projets/dotfiles/zshrc
 cp -v -u ${HOME}/.zsh_history ${HOME}/Documents/mes_projets/dotfiles/zsh-history
 cp -v -u ${HOME}/.vimrc ${HOME}/Documents/mes_projets/dotfiles/vimrc
+cp -v -u -r ${HOME}/.config/terminator ${HOME}/Documents/mes_projets/dotfiles/config/terminator
 
 # Copie du dossier custom de Oh-my-zsh"
 if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/custom" ];then
