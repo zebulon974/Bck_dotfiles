ff2cb16062f75da98d58d11d223083a0f3dd17a4
zebulon974 <zebulonfr@hotmail.fr>
2018-12-20 15:57:23 +0100

    Ajout des options de copie

--- ma_config.sh
+++ ma_config.sh
@@ -8,15 +8,15 @@ else
 	echo "Le dossier de dotfiles existe dejà !"
 fi
 
-cp ${HOME}/.zshrc ${HOME}/Documents/mes_projets/dotfiles/zshrc &
-cp ${HOME}/.zsh_history ${HOME}/Documents/mes_projets/dotfiles/zsh-history &
-cp ${HOME}/.vimrc ${HOME}/Documents/mes_projets/dotfiles/vimrc &
+cp -v -u ${HOME}/.zshrc ${HOME}/Documents/mes_projets/dotfiles/zshrc
+cp -v -u ${HOME}/.zsh_history ${HOME}/Documents/mes_projets/dotfiles/zsh-history
+cp -v -u ${HOME}/.vimrc ${HOME}/Documents/mes_projets/dotfiles/vimrc
 
 # Copie du dossier custom de Oh-my-zsh"
 if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/custom" ];then
-	cp -r ${HOME}/.oh-my-zsh/custom/ ${HOME}/Documents/mes_projets/dotfiles/ &
+	cp -r -v -u ${HOME}/.oh-my-zsh/custom/ ${HOME}/Documents/mes_projets/dotfiles/
 else
 	echo "Le Dossier Custom existe déjà"
 fi
-
-echo "Sauvegarde Treminée!!"
+# sleep 2
+echo "Sauvegarde Terminée!!"
