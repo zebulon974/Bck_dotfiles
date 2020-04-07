bb629c0df3d5f048ef0f1f13c97d76900f61469e
zebulon974 <zebulonfr@hotmail.fr>
2020-04-07 17:30:34 +0200

    Ajout fichier couleur console

--- ma_config.sh
+++ ma_config.sh
@@ -1,35 +1,38 @@
 #!/bin/zsh
 
 # Vérification de la présence du dossier Dotfilkes
-if [ ! -d "${HOME}/Documents/mes_projets/dotfiles" ];then
+if [ ! -d "${HOME}/Projets/dotfiles" ];then
     echo "Création du dossier Dotfiles.";
-    mkdir ${HOME}/Documents/mes_projets/dotfiles/
+    mkdir ${HOME}/Projets/dotfiles/
 else
 	echo "Le dossier dotfiles existe,"
 fi
 
 # Vérification du dossier de Configuration Terminator
-if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/conf_terminator" ];then
+if [ ! -d "${HOME}/Projets/dotfiles/conf_terminator" ];then
     echo "Création du dossier conf-terminator";
-    mkdir ${HOME}/Documents/mes_projets/dotfiles/conf_terminator/
+    mkdir ${HOME}/Projets/dotfiles/conf_terminator/
 else
     echo "le dossier de conf terminator existe déjà, copie des fichiers en cours..."
 fi
 
 # Vérification du dossier custom de Oh-my-zsh"
-if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/custom_oh_my_zsh" ];then
+if [ ! -d "${HOME}/Projets/dotfiles/custom_oh_my_zsh" ];then
     echo "Création du dossier custom de Oh My ZSH";
-    mkdir ${HOME}/Documents/mes_projets/dotfiles/custom_oh_my_zsh/
+    mkdir ${HOME}/Projets/dotfiles/custom_oh_my_zsh/
 else
 	echo "Le Dossier Custom Oh My ZSH existe, copie des fichiers en cours..."
 fi
 
+sleep5
+
 # Copie des fichiers
-cp -v -u ${HOME}/.zshrc ${HOME}/Documents/mes_projets/dotfiles/zshrc
-cp -v -u ${HOME}/.zsh_history ${HOME}/Documents/mes_projets/dotfiles/zsh-history
-cp -v -u ${HOME}/.vimrc ${HOME}/Documents/mes_projets/dotfiles/vimrc
-cp -v -u -r ${HOME}/.config/terminator ${HOME}/Documents/mes_projets/dotfiles/conf_terminator
-cp -r -v -u ${HOME}/.oh-my-zsh/custom/ ${HOME}/Documents/mes_projets/dotfiles/custom_oh_my_zsh
+cp -v -u ${HOME}/.zshrc ${HOME}/Projets/dotfiles/zshrc
+cp -v -u ${HOME}/.zsh_history ${HOME}/Projets/dotfiles/zsh-history
+cp -v -u ${HOME}/.vimrc ${HOME}/Projets/dotfiles/vimrc
+cp -v -u -r ${HOME}/.config/terminator ${HOME}/Projets/dotfiles/conf_terminator
+cp -r -v -u ${HOME}/.oh-my-zsh/custom/ ${HOME}/Projets/dotfiles/custom_oh_my_zsh
 
 sleep 5
+
 echo "Sauvegarde Terminée!!!"
