017107c41db8edf3706372177d047d3af2acc9af
zebulon974 <zebulonfr@hotmail.fr>
2020-04-07 12:56:43 +0200

    Modification du fichier ma_config.sh le 07 04 2020

--- ma_config.sh
+++ ma_config.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
-#Les fichiers à sauvergarder
+# Vérification de la présence du dossier Dotfilkes
 if [ ! -d "${HOME}/Documents/mes_projets/dotfiles" ];then
-    echo "Création du dossier mes_projets/dotfiles !";
+    echo "Création du dossier Dotfiles.";
     mkdir ${HOME}/Documents/mes_projets/dotfiles/
 else
 	echo "Le dossier dotfiles existe,"
 fi
 
-#verification du dossier de conf Terminator
+# Vérification du dossier de Configuration Terminator
 if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/conf_terminator" ];then
-    echo "Création du dossier conf-terminator"
+    echo "Création du dossier conf-terminator";
     mkdir ${HOME}/Documents/mes_projets/dotfiles/conf_terminator/
 else
     echo "le dossier de conf terminator existe déjà, copie des fichiers en cours..."
@@ -18,7 +18,7 @@ fi
 
 # Vérification du dossier custom de Oh-my-zsh"
 if [ ! -d "${HOME}/Documents/mes_projets/dotfiles/custom_oh_my_zsh" ];then
-    echo "Création du dossier custom de Oh My ZSH"
+    echo "Création du dossier custom de Oh My ZSH";
     mkdir ${HOME}/Documents/mes_projets/dotfiles/custom_oh_my_zsh/
 else
 	echo "Le Dossier Custom Oh My ZSH existe, copie des fichiers en cours..."
